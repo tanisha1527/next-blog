16a46aedcc70376d9353b98f425a4c4193c23397
tanisha1527 <vermatanisha620@gmail.com>
2025-02-06 12:09:38 +0530

    Add next-themes to apply dark mode

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,11 +1,14 @@
 'use client';
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Button, Navbar, TextInput, theme } from 'flowbite-react';
 import Link from 'next/link';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
+
 export default function Header() {
   const path = usePathname();
+  const { theme, setTheme } = useTheme();
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -29,8 +32,8 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
         <Link href='/sign-in'>
           <Button gradientDuoTone='purpleToBlue' outline>
